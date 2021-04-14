e4260ad70c62c2cad07d5e5ed71e225cd1813095
MashaaX <rianherlangga61@gmail.com>
2021-04-14 07:58:20 +0700

    update2

--- Week9/web-reactku7/src/container/BlogPost/BlogPost.jsx
+++ Week9/web-reactku7/src/container/BlogPost/BlogPost.jsx
@@ -1,24 +1,23 @@
 import React, {Component} from "react";
 import Post from "../../component/BlogPost/Post";
 import './BlogPost.css';
+import '../../services/index';
 
 class BlogPost extends Component{
     state = {
-        listArtikel: []
-        // insertArtikel: {
-        //     userId: 1,
-        //     id: 1,
-        //     title: "",
-        //     body: ""
-        // }
+        listArtikel: [],
+        insertArtikel: {
+            userId: 1,
+            id: 1,
+            title: "",
+            body: ""
+        }
     }
 
     ambilDataDariServerApi = () => {
-        fetch('http://localhost:3001/posts')
-        .then(Response => Response.json())
-        .then(jsonHasilAmbilDariAPI => {
+        API.getNewsBlog().then(result => {
             this.setState({
-                listArtikel: jsonHasilAmbilDariAPI
+                listArtikel: result
             })
         })
     }
@@ -34,13 +33,52 @@ class BlogPost extends Component{
             })
     }
 
+    handleTambahArtikel = (event) =>{
+        let formInsertArtikel = {...this.state.insertArtikel};
+        let timestamp = new Date().getTime();
+        formInsertArtikel['id'] = timestamp;
+        formInsertArtikel[event.target.name] = event.target.value;
+        this.setState({
+            insertArtikel: formInsertArtikel
+        })
+    }
+
+    handleTombolSimpan = () => {
+        fetch('http://localhost:3001/posts',{
+            method: 'post',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(this.state.insertArtikel)
+        })
+            .then((response)=>{
+                this.ambilDataDariServerAPI();
+            });
+    }
+
     render(){
         return(
             <div className="post-artikel">
-                <h2>Daftar Artikel</h2>
+                <div className="form pb-2 border-bottom">
+                    <div className="form-group row">
+                        <label htmlFor="title" className="col-sm2 col-form-label">Judul</label>
+                        <div className="col-sm-10">
+                            <input type="text" className="form-control" id="title" name="title" onChange={this.handleTambahArtikel}/>
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <label htmlFor="body" className="col-sm-2 col-form-label">Isi</label>
+                        <div className="col-sm-10">
+                            <textarea className="form-control" id="body" name="body" rows="3" onChange={this.handleTambahArtikel}></textarea>
+                        </div>
+                    </div>
+                    <button type="submit" className="btn btn-primary" onClick={this.handleTombolSimpan}>Simpan</button>
+                </div>
+                <h2>Article List</h2>
                 {
                     this.state.listArtikel.map(artikel => {
-                        return <Post key={artikel.id} judul={artikel.title} isi={artikel.body} idArtikel={artikel.id} hapusArtikel={this.handleHapusArtikel}/> 
+                        return <Post key={artikel.id} judul={artikel.title} isi={artikel.body} idArtikel={artikel.id} hapusArtikel={this.handleHapusArtikel}/>
                     })
                 }
             </div>
