0d124b0cc67fad9b21fb33a47d9e79bf25f4ddac
MashaaX <rianherlangga61@gmail.com>
2021-05-04 21:26:30 +0700

    week 12 Update

--- Week12/reactjs/src/container/BlogPost/BlogPost.jsx
+++ Week12/reactjs/src/container/BlogPost/BlogPost.jsx
@@ -1,81 +1,168 @@
 import React, {Component} from "react";
 import Post from "../../component/BlogPost/Post";
 import './BlogPost.css';
+import firebase from "firebase";
+import firebaseConfig from "../../firebase/config"
 import API from "../../services/index";
 
 class BlogPost extends Component{
-    state = {
-        listArtikel: [],
-        insertArtikel: {
-            userId: 1,
-            id: 1,
-            title: "",
-            body: ""
+    constructor(props) {
+        super(props);
+    
+        if(!firebase.apps.length){
+          firebase.initializeApp(firebaseConfig);
         }
-    }
-
-    ambilDataDariServerApi = () => {
-        API.getNewsBlog().then(result => {
-            this.setState({
-                listArtikel: result
-            })
-        })
-    }
-
-    componentDidMount(){
-        this.ambilDataDariServerApi()
-    }
-
-    handleHapusArtikel = (data) =>{
-        fetch(`http://localhost:3001/posts/${data}`,{method:'DELETE'})
-            .then(res =>{
-                this.jsonHasilAmbilDariAPI()
-            })
-    }
-
-    handleTambahArtikel = (event) =>{
-        let formInsertArtikel = {...this.state.insertArtikel};
-        let timestamp = new Date().getTime();
-        formInsertArtikel['id'] = timestamp;
-        formInsertArtikel[event.target.name] = event.target.value;
-        this.setState({
-            insertArtikel: formInsertArtikel
-        })
-    }
-
-    handleTombolSimpan = () => {
-        API.postNewsBlog(this.state.insertArtikel)
-            .then((response) => {
-                this.ambilDataDariServerAPI();
-            });
-    }
-
-    render(){
-        return(
-            <div className="post-artikel">
-                <div className="form pb-2 border-bottom">
-                    <div className="form-group row">
-                        <label htmlFor="title" className="col-sm2 col-form-label">Judul</label>
-                        <div className="col-sm-10">
-                            <input type="text" className="form-control" id="title" name="title" onChange={this.handleTambahArtikel}/>
+        
+        this.state = {
+          listArticle: [],
+        };
+      }
+    
+      fetchDataFromAPI = () => {
+        let ref = firebase.database().ref("/");
+        ref.on("value", (snapshot) => {
+          const state = snapshot.val();
+          this.setState(state);
+        });
+      };
+    
+      saveDataToServerAPI = () => {
+        firebase.database().ref("/").set(this.state);
+      };
+    
+      componentDidMount() {
+        this.fetchDataFromAPI();
+      }
+    
+      componentDidUpdate(prevProps, prevState) {
+        if (prevState !== this.state) {
+          this.saveDataToServerAPI();
+          console.log(this.state)
+        }
+      }
+    
+      handleDelete = (idArticle) => {
+        const { listArticle } = this.state;
+        const newState = listArticle.filter((data) => {
+          return data.uid !== idArticle;
+        });
+        this.setState({ listArticle: newState });
+      };
+    
+      handleSave = (event) => {
+        let title = this.refs.articleTitle.value;
+        let body = this.refs.articleBody.value;
+        let uid = this.refs.uid.value;
+    
+        if (uid && title && body) {
+          const { listArticle } = this.state;
+          const articleIndex = listArticle.findIndex((data) => {
+            return data.uid === uid;
+          });
+          listArticle[articleIndex].title = title;
+          listArticle[articleIndex].body = body;
+          this.setState({ listArticle });
+        } else if (title && body) {
+          const uid = new Date().getTime().toString();
+          const { listArticle } = this.state;
+          listArticle.push({ uid, title, body });
+          this.setState({ listArticle });
+        }
+        this.refs.articleTitle.value = "";
+        this.refs.articleBody.value = "";
+        this.refs.uid.value = "";
+      };
+    
+      render() {
+        return (
+          <section id="work" className="portfolio-mf sect-pt4 route">
+            <div className="container">
+              <div className="row">
+                <div className="col-sm-10" style={{ marginLeft: 80 }}>
+                  <div className="contact-mf">
+                    <div id="contact" className="box-shadow-full">
+                      <div className="row">
+                        <div>
+                          <form className="contactForm">
+                            <div className="row">
+                              <div className="col-md-12 mb-3">
+                                <h5 className="title-left">Add Article</h5>
+                              </div>
+                              <div
+                                className="col-md-12 mb-3"
+                                style={{ marginLeft: 30 }}
+                              >
+                                <div className="form-group">
+                                  <input
+                                    type="text"
+                                    name="title"
+                                    className="form-control"
+                                    id="title"
+                                    placeholder="Title"
+                                    ref="articleTitle"
+                                  />
+                                </div>
+                              </div>
+                              <div
+                                className="col-md-12 mb-3"
+                                style={{ marginLeft: 30 }}
+                              >
+                                <div className="form-group">
+                                  <textarea
+                                    className="form-control"
+                                    name="uid"
+                                    rows={5}
+                                    data-rule="required"
+                                    placeholder="Body"
+                                    ref="articleBody"
+                                  />
+                                </div>
+                              </div>
+                              <input type='hidden' name='uid' ref='uid'/>
+                              <div className="col-md-12">
+                                <p className="s-description text-right">
+                                  <button
+                                    className="button button-a button-big button-rouded"
+                                    onClick={this.handleSave}
+                                  >
+                                    Save
+                                  </button>
+                                </p>
+                              </div>
+                            </div>
+                          </form>
                         </div>
+                      </div>
                     </div>
-                    <div className="form-group row">
-                        <label htmlFor="body" className="col-sm-2 col-form-label">Isi</label>
-                        <div className="col-sm-10">
-                            <textarea className="form-control" id="body" name="body" rows="3" onChange={this.handleTambahArtikel}></textarea>
-                        </div>
-                    </div>
-                    <button type="submit" className="btn btn-primary" onClick={this.handleTombolSimpan}>Simpan</button>
+                  </div>
                 </div>
-                <h2>Article List</h2>
-                {
-                    this.state.listArtikel.map(artikel => {
-                        return <Post key={artikel.id} judul={artikel.title} isi={artikel.body} idArtikel={artikel.id} hapusArtikel={this.handleHapusArtikel}/>
-                    })
-                }
+                <div className="col-sm-12">
+                  <div className="title-box text-center">
+                    <h3 className="title-a">List Article</h3>
+                    <p className="subtitle-a">Praktikum Modul 12</p>
+                    <div className="line-mf" />
+                  </div>
+                </div>
+              </div>
+              <div className="row">
+                {/* post */}
+                {this.state.listArticle.map((article) => {
+                  return (
+                    <Post
+                      key={article.uid}
+                      idArticle={article.uid}
+                      title={article.title}
+                      content={article.body}
+                      onDelete={(id) => this.handleDelete(id)}
+                    />
+                  );
+                })}
+                {/* end post */}
+              </div>
             </div>
-        )
+          </section>
+        );
+      }
     }
-}
-export default BlogPost;
+    
+    export default BlogPost;
---
+++ Week12/reactjs/src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAes5hycFPiggVtpwkwIOsNpuzioJS82F0",
+    authDomain: "week11-2203f.firebaseapp.com",
+    projectId: "week11-2203f",
+    storageBucket: "week11-2203f.appspot.com",
+    messagingSenderId: "579448499888",
+    appId: "1:579448499888:web:f469e2af55a8ce86c9d575",
+    measurementId: "G-Q8Q3L2WC3B"
+  };
+
+export default firebaseConfig;
