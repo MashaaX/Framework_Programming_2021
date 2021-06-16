c17820de3db0a513e9ae0943147465f9cc5cfc51
MashaaX <rianherlangga61@gmail.com>
2021-06-17 05:08:11 +0700

    Update Cart and UI

--- UAS/the-essentials-app/src/components/Navbar/Navbar.jsx
+++ UAS/the-essentials-app/src/components/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
             <AppBar position = "fixed" className={classes.appBar} color="inherit">
                 <Toolbar>
                     <Typography variant="h6" className={classes.title} color="inherit">
-                        <img src={logo} alt="The-Essentials" height="25px" className={classes.image}/>
+                        <img src={logo} alt="commerce.js" height="25px" className={classes.image}/>
                         E s s e n t i a l s
                     </Typography>
                     <div className={classes.grow} />
--- UAS/the-essentials-app/src/components/Products/Products.jsx
+++ UAS/the-essentials-app/src/components/Products/Products.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
 
-import Product from './Product/Product'
+import Product from './Product/Product';
+import useStyles from './style';
 
-const products = [
-    { id: 1, name: 'Shoes', description: 'Yeezy', price: 'Rp.8000', image: 'https://cdn.shopify.com/s/files/1/0255/9429/8467/products/yeezy-450-cloud-white-h68038_6_umreo3_1800x1800.jpg'},
-    { id: 2, name: 'Gun', description: 'Mac-10', price: 'Rp.9000', image: 'https://photo.weaponsystems.net/image/s-lightbox/n-MAC-10/--/img/ws/fa_smg_mac10_v1.jpg'},
-];
 
-const Products = () => {
+const Products = ({products, onAddToCart}) => {
+    const classes = useStyles();
+
     return(
-        <main>
+        <main className={classes.content}> 
+        <div className={classes.toolbar} />
             <Grid container justify="center" spacing={4}>
                 {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                        <Product product = { product } />
+                        <Product product = { product } onAddToCart={onAddToCart}/>
                     </Grid>
                 ) )}
             </Grid>
